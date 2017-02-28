26d56694a589a7418369893906afed173c51afaa
aya <ayaasfour64@gmail.com>
2017-02-28 17:34:15 +0200

    add lexicon search component

---
+++ src/screens/LexiconSearch.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="lexiconSearch">
+    <headerComponent></headerComponent>
+    <LexiconSearchComponent></LexiconSearchComponent>
+  </div>
+</template>
+
+<script>
+import HeaderComponent from '../components/HeaderComponent'
+import LexiconSearchComponent from '../components/LexiconSearchComponent'
+export default {
+  name: 'lexiconSearch',
+  components: {
+   HeaderComponent,LexiconSearchComponent
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods:{
+
+}
+}
+</script>
+
+
+<style scoped>
+
+</style>
